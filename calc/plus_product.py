4768c2ab7d349b5e0dacc6d9a2358bbafc35b88d
Younggwang Kim <dudrhkd75@gmail.com>
2020-06-26 15:47:00 +0900

    Issue #3 Add WSGI file in github

---
+++ calc/plus_product.py
@@ -0,0 +1,22 @@
+from cgi import parse_qs
+from plus_product_template import html
+
+def application(environ, start_response):
+    c = parse_qs(environ['QUERY_STRING'])
+    a = c.get('a', [''])[0]
+    b = c.get('b', [''])[0]
+    x = 0
+    y = 0
+    if '' not in [a, b]:
+        a, b = int(a), int(b)
+        x = a + b
+        y = a * b
+    response_body = html % {
+        'x' : x or '',
+        'y' : y or '',
+        }
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
---
+++ calc/plus_product_template.py
@@ -0,0 +1,16 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            A : <input type="number" name="a"><br>
+            B : <input type="number" name="b"><br><br>
+            <input type="submit"><br><br>
+        </form>
+        <p>
+        sum : %(x)s<br>
+        product : %(y)s<br>
+        </p>
+    </body>
+</html>
+"""
+
